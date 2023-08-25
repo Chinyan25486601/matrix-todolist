a23576c3e94184ae62e311cc274669d189e395e3
Chinyan <2640855495@qq.com>
2023-08-25 22:02:25 +0800

    进行了更多响应式布局

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@ import CircleButton from './components/CircleButton.vue'
             <RouterView></RouterView>
         </Card>
         <div class="buttons">
+            <div class="apptitle">矩待办</div>
             <CircleButton>+</CircleButton>
         </div>
     </div>
@@ -23,12 +24,40 @@ import CircleButton from './components/CircleButton.vue'
 .main>* {
     margin: 8px;
 }
+.buttons {
+    display: flex;
+}
+.apptitle {
+    font-size: 30px;
+}
+
+@media (min-width: 768px) {
+    .buttons {
+        flex-direction: column;
+        height: 800px;
+        justify-content: center;
+    }
+    .apptitle {
+        writing-mode: vertical-lr;
+        margin-bottom: auto;
+        margin-top: 0px;
+    }
+}
 
 @media (max-width: 768px) {
   /* 在小屏幕下调整Card的样式 */
   .main {
     flex-direction: column;
     align-items: start;
+  }
+  .buttons {
+    width: 100%;
+    justify-content: center;
+    flex-direction: row-reverse;
+  }
+  .apptitle {
+    margin-left: auto;
+    margin-right: 0px;
   }
 }
 </style>
